ced78971a0b56e514ad19d44a19b4f0a01186667
Vinicius Alves Dias <alvesdias0008@outlook.com>
2025-07-31 17:24:28 -0300

    Primeiro commit do projeto

---
+++ src/ConversaoJsonParaObjeto.java
@@ -0,0 +1,12 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class ConversaoJsonParaObjeto {
+    public static void main(String[] args) {
+        String json = "{\"nome\":\"Rodrigo\",\"idade\":20,\"cidade\":\"Brasília\"}";
+
+        Gson gson = new GsonBuilder().serializeNulls().create();
+        Pessoa pessoa = gson.fromJson(json, Pessoa.class);
+        System.out.println("Objeto Pessoa: " + pessoa);
+    }
+}
